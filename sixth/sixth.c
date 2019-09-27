cad38123e49e74c86b15df218d4724e1d87f3bd5
Sujit Molleti <sujit.molleti@rutgers.edu>
2019-09-27 19:52:17 -0400

    working on just make

--- sixth/sixth.c
+++ sixth/sixth.c
@@ -2,6 +2,20 @@
 #include<stdio.h>
 #include<string.h>
 
+
+/*
+
+Hello and welcome to Computer Architecture
+elloHay andyay elcomeway otay omputerCay Architectureyay
+This is just a test to make sure your program is running correctly
+isThay isyay ustjay ayay esttay otay akemay uresay ouryay ogrampray isyay unningray orrectlycay
+a
+ayay
+bye
+ebyay
+
+*/
+
 char* shift(char*);
 
 char* shift(char* str){
@@ -19,7 +33,7 @@ char* shift(char* str){
 
 int main(int argc, char** argv){
 
-	char vowels[] = {'a', 'e', 'i', 'o', 'u'};
+	char vowels[] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
 
 	for(int i = 1; i<argc; ++i){
 
@@ -27,10 +41,10 @@ int main(int argc, char** argv){
 		char * str = argv[i];
 		char temp = str[0];
 
-		for(int i = 0; i<5; ++i){
+		for(int i = 0; i<10; ++i){
 			if(str[0] == vowels[i]){
 				break;
-			}else if(i == 4){
+			}else if(i == 9){
 				//here: it means that the first letter is not a vowel
 				str = shift(str);
 				/* we just shifted but we don't know 
@@ -43,17 +57,20 @@ int main(int argc, char** argv){
 
 		printf("%s", str);
 
-		for(int i = 0; i<5; ++i){
+		for(int i = 0; i<10; ++i){
 			if(temp == vowels[i]){
-				printf("yay ");
+				printf("yay");
+				if(i != (argc-1)){
+					printf(" ");
+				}
 				break;
-			} else if (i == 4){
-				printf("ay ");
+			} else if (i == 9){
+				printf("ay");
+				if(i != (argc-1)){
+					printf(" ");
+				}
 			}
 		}
-
-
-
 
 	}
 
