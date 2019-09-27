8345eb3d7704d8f147457d8bca8628bfdd46c4ea
Sujit Molleti <sujit.molleti@rutgers.edu>
2019-09-27 19:31:27 -0400

    finished according to directions

--- sixth/sixth.c
+++ sixth/sixth.c
@@ -2,22 +2,44 @@
 #include<stdio.h>
 #include<string.h>
 
+char* shift(char*);
+
+char* shift(char* str){
+
+	char temp = str[0];
+
+	for(int j = 1; j<strlen(str); ++j){
+		str[j-1] = str[j];
+	}
+	str[strlen(str)-1] = temp;
+
+	return str;
+
+}
+
 int main(int argc, char** argv){
 
-	 char vowels[] = {'a', 'e', 'i', 'o', 'u'};
+	char vowels[] = {'a', 'e', 'i', 'o', 'u'};
 
 	for(int i = 1; i<argc; ++i){
 
 
 		char * str = argv[i];
-
 		char temp = str[0];
 
-		for(int j = 1; j<strlen(str); ++j){
-			str[j-1] = str[j];
+		for(int i = 0; i<5; ++i){
+			if(str[0] == vowels[i]){
+				break;
+			}else if(i == 4){
+				//here: it means that the first letter is not a vowel
+				str = shift(str);
+				/* we just shifted but we don't know 
+					if the first letter is a vowel, so restart
+				*/
+				i = 0;
+			}
 		}
 
-		str[strlen(str)-1] = temp;
 
 		printf("%s", str);
 
