f5a9065c872397105135118a98714e7f158be1b8
Sujit Molleti <sujit.molleti@rutgers.edu>
2019-09-30 02:23:24 -0400

    Done with three

--- third/test.c
+++ third/test.c
@@ -1,5 +1,4 @@
 //third project
-
 #include<stdlib.h>
 #include<stdio.h>
 
@@ -7,10 +6,7 @@ int hashfunction(int);
 void insert(int,int);
 int search(int);
 
-
 struct node* arr[1000];
-
-//ListNode[] x = new 
 
 struct node{
 	int value;
@@ -18,8 +14,13 @@ struct node{
 };
 
 int hashfunction(int value){
+	int temp = value;
 
-	return value%1000;
+	if(value<0){
+		temp = temp * -1;
+	}
+
+	return temp%1000;
 }
 
 //INSERT METHOD
@@ -30,15 +31,26 @@ void insert(int value, int hc){
 		printf("duplicate\n");
 		return;
 	}
-
-
 	//printf("value: %d\thashcode: %d\n", value, hc);
 
 	struct node* temp = malloc(sizeof(struct node)); //making space for node
 	temp->value = value;
 	temp->next = NULL;
 
-	arr[hc] = temp;
+	if(arr[hc] == NULL){
+		arr[hc] = temp;
+		printf("inserted\n");
+		return;
+	}
+
+	struct node* ptr = arr[hc];
+
+	while(ptr->next != NULL){
+		ptr = ptr->next;
+	}
+
+	ptr->next = temp;
+
 
 	//printf("%d--->inserting into %d\n", arr[hc]->value, hc);
 
@@ -51,33 +63,30 @@ int search(int value){
 
 	struct node* ptr = arr[hc];
 
-	//printf("%d--->searching for in this index %d\n", ptr->value, hc);
-
+	//printf("%d--->searching for in this index %d\n", ptvalue, hc);
 
 	while(ptr != NULL){
+		//printf("search route: %d ", ptr->value);
+
 		if(ptr->value == value){
 			return 0;
 		}
-
 		ptr = ptr->next;
 	}
-
+	//printf("\n");
 	return -1;
 }
 
 int main(int argc, char** argv){
-
 	if(argc != 2){
 		printf("insufficient arguments\n");
 		exit(0);
 	}
-
 	FILE* fp = fopen(argv[1], "r");
 	if(fp == NULL){
 		printf("error\n");
 		exit(0);
 	}
-
 	int num;
 	char c;
 	int hc;
@@ -85,7 +94,6 @@ int main(int argc, char** argv){
 	while(fscanf(fp, "%c\t%d\n", &c, &num) > 0){
 		// printf("Values: %c\t%d\t hashcode: %d\n", c, num, hashfunction(num));
 		hc = hashfunction(num);
-
 		if(c == 'i'){
 			insert(num, hc);
 		}
@@ -96,7 +104,6 @@ int main(int argc, char** argv){
 				printf("absent\n");
 			}
 		}
-
 	}
 
 	//int x = 14;
